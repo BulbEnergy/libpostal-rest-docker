c03812954aa9fda0762666c5c8f4d5e1e9b0295b
harrygr <harry@bulb.co.uk>
2021-03-29 11:46:07 +0100

    Add kubernetes deploy

--- .circleci/gcloud-auth.sh
+++ .circleci/gcloud-auth.sh
@@ -16,12 +16,18 @@ if [ ! -z $PROJECT_NAME ]; then
   GCLOUD_PROJECT="$(gcloud projects list --format="value(projectId)" --filter="name=${PROJECT_NAME}")"
   gcloud config set project ${GCLOUD_PROJECT}
 
+  GCLOUD_CLUSTER=$(gcloud container clusters list --format="value(name)" --filter="name ~ ${PROJECT_NAME}")
+  GCLOUD_ZONE=$(gcloud container clusters list --format="value(location)" --filter="name ~ ${PROJECT_NAME}")
 fi
 
 # GCLOUD_ZONE is defined in the `bulbenrg-default` context but if context isn't used fall back to the default value
 GCLOUD_ZONE=${GCLOUD_ZONE:-"europe-west1-c"}
 
+gcloud container clusters get-credentials ${GCLOUD_CLUSTER} --zone ${GCLOUD_ZONE} --project ${GCLOUD_PROJECT}
+
+echo "export GCLOUD_CLUSTER=${GCLOUD_CLUSTER}" >> $BASH_ENV
 echo "export GCLOUD_PROJECT=${GCLOUD_PROJECT}" >> $BASH_ENV
+echo "export GCLOUD_ZONE=${GCLOUD_ZONE}" >> $BASH_ENV
 
 gcloud config set project ${GCLOUD_PROJECT}
 gcloud auth configure-docker
